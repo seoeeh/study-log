aacc669c0d9ac694c9d75bad4404f87338d16d81
Seohee Park <kelly2650@daum.net>
2026-02-06 19:54:03 +0900

    1.7 지역 범위

---
+++ Inflearn/chap1_7/chap1_7.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+
+int main()
+{
+	int x = 0;
+
+	cout << x <<" " << &x << endl; //&: 메모리 주소
+
+
+	{
+		//int x = 0;
+
+		x = 1;
+
+		cout << x << " " << &x << endl;
+
+	} //중괄호 역할: 영역분리
+
+
+	cout << x << " " << &x << endl;
+
+
+	return 0;
+
+}
+
+*/
---
+++ Inflearn/chap1_7/chap1_7_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void doSomething(int x)
+{
+	x = 123;
+	cout << x << endl; // #2
+}
+
+int main()
+{
+	int x = 0;
+
+	cout << x << endl; // #1
+	doSomething(x);
+	cout << x << endl; // #3
+
+	return 0;
+}
